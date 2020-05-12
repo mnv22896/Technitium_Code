e424c275d1167affe844a8bfa84061024b8e1400
mnv22896 <47471531+mnv22896@users.noreply.github.com>
2020-05-12 23:15:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+
+                            Online Python Interpreter.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+quantity={large:10,Xlarge:20,2Xlarger:40,4Xlarge:80,8Xlarge:160,10Xlarge}
+New_rate={large:120,Xlarge:230,2Xlarge:450,6Xlarge:774,8Xlarge:1400,10Xlarge:2820}
+India_rate={large:140,2Xlarge:413,6Xlarge:890,8Xlarge:1300,10Xlarge:2970}
+China_rate={large:120,Xlarge:230,2Xlarge:450,6Xlarge:774,8Xlarge:1400,10Xlarge:2820}
