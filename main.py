cd522e1cef11bfbede68137b1a9e314ce407f7fd
mnv22896 <47471531+mnv22896@users.noreply.github.com>
2020-05-12 23:18:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Write your code in this editor and press "Run" button to execute it.
 
 '''
 quantity={large:10,Xlarge:20,2Xlarger:40,4Xlarge:80,8Xlarge:160,10Xlarge}
-New_rate={large:120,Xlarge:230,2Xlarge:450,6Xlarge:774,8Xlarge:1400,10Xlarge:2820}
-India_rate={large:140,2Xlarge:413,6Xlarge:890,8Xlarge:1300,10Xlarge:2970}
-China_rate={large:120,Xlarge:230,2Xlarge:450,6Xlarge:774,8Xlarge:1400,10Xlarge:2820}
+New_rate={large:120,Xlarge:230,2Xlarge:450,4Xlarge:774,8Xlarge:1400,10Xlarge:2820}
+India_rate={large:140,2Xlarge:413,4Xlarge:890,8Xlarge:1300,10Xlarge:2970}
+China_rate={large:110,Xlarge:200,4Xlarge:670,8Xlarge:1180}
+
