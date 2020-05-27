910ac3b8179b1c17df427c4767d48a9056b560d9
mnv22896 <47471531+mnv22896@users.noreply.github.com>
2020-05-28 03:58:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-
 from collections import defaultdict
 import math
 quantity=['large','Xlarge','X2large','X4large','X8large','X10large']
 qua=[10,20,40,80,160,320]
-New_rate={'large':120,'Xlarge':230,'Xlarge':450,'X4large':774,'X8large':1400,'X10large':2820}
-India_rate={'large':140,'X2large':413,'X4large':890,'X8large':1300,'X10large':2970}
-China_rate={'large':110,'Xlarge':200,'X4large':670,'X8large':1180}
+India_qua=[10,40,80,160,320]
+china_qua=[10,20,80,160]
+New_quantity=['large','Xlarge','X2large','X4large','X8large','X10large']
+India_quantity=['large','Xlarge','X2large','X4large','X8large','X10large']
+china_quantity=['large','Xlarge','X4large','X8large']
+New_rate=[120,230,450,774,1400,2820]
+India_rate=[140,413,890,1300,2970]
+China_rate=[110,200,670,1180]
 ans=defaultdict()
 capacity=1150
 hour=1 
-output=defaultdict(tuple)
-p=0
-def cost(capacity,hour,ouput,qua,quantity,ans):
-    
-   
-    
+minimum=defaultdict()
+global p
+p=100000
+def cost(capacity,hour,qua,quantity,ans):
+    global p
     for i in range(0,len(qua)):
-        print(qua[-i-1],capacity,i,qua)
         
         
         if capacity%qua[-i-1] ==0:
             ans[quantity[-i-1]]=int(math.floor(capacity/qua[-i-1])) 
-            print(ans,capacity,quantity[-1-i])
-            output[i]=ans
+            output=amount(ans)
+            if p > output :
+                p=output
+                minimum[p]=ans
             ans[quantity[-i-1]]=0
             return 
             
         elif capacity > qua[-i-1]:
             ans[quantity[-i-1]]=int(math.floor(capacity/qua[-i-1])) 
-            cost(capacity%qua[-i-1],hour,ouput,qua[:-i-1],quantity[:-i-1],ans)
+            cost(capacity%qua[-i-1],hour,qua[:-i-1],quantity[:-i-1],ans)
             ans[quantity[-i-1]]=0
+
             
-            
-t=cost(capacity,hour,output,qua,quantity,ans)
-print(output)
-            
-        
+def amount(ans):
+    sum=0
+    for i in ans:
+        sum=sum + (ans[i] * New_rate[New_quantity.index(i)])
+    return sum    
+t=cost(capacity,hour,qua,quantity,ans)
+
+print(p)
